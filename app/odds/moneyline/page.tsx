90a519c96109812111a4d09355ce2c07313a4aea
Jacob Greene <jepgpersonal@gmail.com>
2025-04-01 08:31:04 -0400

    re-commit, same as previous - route updates -jg

---
+++ app/odds/moneyline/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/_Header";
+import Footer from "@/app/components/_Footer";
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <div className="bg-cover bg-center min-h-screen flex flex-col items-center justify-center">
+                <h1 className="text-4xl font-bold text-center text-white">Welcome to the betting analysis on moneyline bets.</h1>
+            </div>
+            <Footer />
+        </div>
+    )
+}
