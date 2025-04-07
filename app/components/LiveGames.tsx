42eaf3b9d4226623e7235e69f80028c5a023c65d
Alarman-prg <jepgpersonal@gmail.com>
2025-04-06 22:15:30 -0400

    Index games changes

--- app/components/LiveGames.tsx
+++ app/components/LiveGames.tsx
@@ -30,7 +30,7 @@ export default function LiveGames({ numberOfGames }: LiveGamesProps) {
       <div className='flex items-center justify-between text-white text-3xl font-bold pl-5 pt-5 pb-2'>
         <div className='flex-col items-center'>
           <div className='flex items-center p-4'>
-            <h1 className='text-5xl'>Upcoming Games</h1>
+            <h1 className='text-5xl'>Games this season</h1>
           </div>
           <div className='p-4'>
             <button className='bg-sky-500 text-black px-4 py-1 rounded-lg shadow-md hover:bg-sky-900 text-2xl'>
@@ -38,19 +38,19 @@ export default function LiveGames({ numberOfGames }: LiveGamesProps) {
             </button>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8 px-10 py-8 place-items-center">
-          {games.slice(0, numberOfGames).map((game) => (
-            <LiveGame
-              key={game.id}
-              homeTeam={game.home_team}
-              awayTeam={game.away_team}
-              commenceTime={game.commence_time}
-            />
-          ))}
+        <div className='m-2 p-2'>
+          <h1 className='text-center text-5xl font-bold'>Games happening soon</h1>
+          <div className="grid grid-row-1 sm:grid-row-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-10 py-8 place-items-center">
+            {games.slice(0, numberOfGames).map((game) => (
+              <LiveGame
+                key={game.id}
+                homeTeam={game.home_team}
+                awayTeam={game.away_team}
+                commenceTime={game.commence_time}
+              />
+            ))}
+          </div>
         </div>
-
-
       </div>
     </div>
   );
