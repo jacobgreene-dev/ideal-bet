f2f1c0d2e241b104c030d2bd29973d3c71560dc2
Alarman-prg <jepgpersonal@gmail.com>
2025-04-22 01:43:40 -0400

    standardized sizing

--- app/components/TeamCDNLogo.tsx
+++ app/components/TeamCDNLogo.tsx
@@ -4,17 +4,21 @@ import Image from 'next/image';
 import { formatTeamNameForCDN } from '@/lib/utils/formatTeamCDN';
 import { TeamCDNLogoProps } from '@/lib/types/apiTypes';
 
-export function TeamCDNLogo({ teamName, size = 80 }: TeamCDNLogoProps) {
+export function TeamCDNLogo({ teamName, size = 100 }: TeamCDNLogoProps) {
   const formattedName = formatTeamNameForCDN(teamName);
   const src = `https://i.logocdn.com/nba/2024/${formattedName}.svg`;
 
   return (
-    <Image
-      src={src}
-      alt={`${teamName} logo`}
-      width={size}
-      height={size}
-      className="rounded"
-    />
+    <div
+      className="relative bg-white rounded-full overflow-hidden flex items-center justify-center border shadow"
+      style={{ width: size, height: size }}
+    >
+      <Image
+        src={src}
+        alt={`${teamName} logo`}
+        fill
+        className="object-contain p-2"
+      />
+    </div>
   );
 }
