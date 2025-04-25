ee1268873ff06538f625096f7033168421f2cc1f
Alarman-prg <jepgpersonal@gmail.com>
2025-04-25 01:04:01 -0400

    logo update && css tweaks in header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -34,9 +34,12 @@ import {
 const products = [
   { name: 'Bet Analysis', description: 'Moneyline, Spread, and Over/Under', href: '/analysis', icon: Vr },
   { name: 'Saved Bets', description: 'Past bet analysis computations', href: '/savedbets', icon: ArchiveBoxIcon },
+];
+
+const NBA = [
   { name: 'Players', description: 'Gain insight on performances of your favorite players.', href: '/players', icon: Person },
   { name: 'Teams', description: 'Build knowledge on competing teams.', href: '/teams', icon: People }
-];
+]
 
 export default function Header() {
   const { data: session } = useSession();
@@ -59,29 +62,29 @@ export default function Header() {
           <Link href='/'>
 
             {/* Replace with up-to-date logo */}
-            
+
             <Image
-              width={40}
-              height={40}
+              width={100}
+              height={100}
               src="/logo.png"
               priority
               unoptimized
               alt="Ideal Strategies Logo"
-              className="h-11 max-h-12 w-auto"
+              className="h-16 w-auto"
             />
           </Link>
         </div>
 
         {/* Mobile Menu Button */}
         <div className="flex sm:hidden">
-        <button
-  type="button"
-  onClick={() => setMobileMenuOpen(true)}
-  className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
->
-  <span className="sr-only">Open main menu</span>
-  <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-</button>
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
 
         </div>
 
@@ -90,11 +93,40 @@ export default function Header() {
           <PopoverGroup className="hidden sm:flex sm:gap-x-8">
             <Popover className="relative">
               <PopoverButton className="flex items-center gap-x-1 text-md text-white hover:text-gray-400">
+                NBA Insights
+                <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-white" />
+              </PopoverButton>
+
+              <PopoverPanel className="absolute top-full z-10 mt-2 w-screen max-w-md overflow-hidden rounded-3xl bg-[#2b3340] ring-1 shadow-lg ring-white/5">
+                <div className="p-4">
+                  {NBA.map((item) => (
+                    <div
+                      key={item.name}
+                      className="relative flex items-start gap-x-6 rounded-lg p-4 text-sm transition-all duration-300 hover:bg-gray-700 hover:scale-105 hover:shadow-lg"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-black" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block text-white">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-white">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </PopoverPanel>
+            </Popover>
+
+            <Popover className="relative">
+              <PopoverButton className="flex items-center gap-x-1 text-md text-white hover:text-gray-400">
                 Betting Odds Analysis
                 <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-white" />
               </PopoverButton>
 
-              <PopoverPanel className="absolute top-full z-10 mt-2 w-screen max-w-md overflow-hidden rounded-3xl bg-gray-700 ring-1 shadow-lg ring-white/5">
+              <PopoverPanel className="absolute top-full z-10 mt-2 w-screen max-w-md overflow-hidden rounded-3xl bg-[#2b3340] ring-1 shadow-lg ring-white/5">
                 <div className="p-4">
                   {products.map((item) => (
                     <div
@@ -123,9 +155,10 @@ export default function Header() {
           </PopoverGroup>
         )}
 
+
         {session ? (
           <div className="hidden sm:flex sm:items-center sm:space-x-4 sm:flex-1 sm:justify-end">
-            <div className="flex items-center space-x-2 bg-black/60 px-3 py-1 rounded-lg shadow-sm border border-gray-700">
+            <div className="flex items-center space-x-2 bg-gray-800/60 px-3 py-1 rounded-lg shadow-sm border border-gray-700">
               <Image
                 src={session.user?.image || ''}
                 alt="Profile"
@@ -146,7 +179,7 @@ export default function Header() {
           <div className="hidden sm:flex sm:flex-1 sm:justify-end">
             <button
               onClick={() => signIn('google')}
-              className="text-sm text-white bg-blue-600 hover:bg-blue-700 px-4 py-1 rounded-lg transition"
+              className="text-sm text-white bg-sky-500 hover:bg-sky-600 px-4 py-2 rounded-full shadow-lg transition-transform transform hover:scale-105 hover:text-bold"
             >
               Log in
             </button>
@@ -157,7 +190,7 @@ export default function Header() {
       {/* Mobile Menu Panel */}
       {isClient && (
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="sm:hidden">
-          <div className="fixed inset-0 z-10"/>
+          <div className="fixed inset-0 z-10" />
           <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
